eadefdd9383c9d474f6d628ff3bd6db03296b1a5
KeenSpirit <96152955+KeenSpirit@users.noreply.github.com>
2026-06-08 17:14:33 +1000

    dev update 18

--- ui/obtain_all_grids.py
+++ ui/obtain_all_grids.py
@@ -18,3 +18,26 @@ def all_egx_grids(app):
     exg_grids_sorted = sorted(egx_grids, key=lambda grid: grid.GetAttribute('loc_name'))
 
     return exg_grids_sorted
+
+
+def all_ca_grids(app):
+    pass
+
+    try:
+        ee_obj_fold = app.GetProjectFolder("netdat").GetContents("Energex_Master")[0]
+    except AttributeError:
+        app.PrintPlain("Could not locate Energex_Master folder. Check the TransSubtrans project is Active.")
+        sys.exit(0)
+
+
+def all_fn_grids(app):
+    pass
+
+def all_mk_grids(app):
+    pass
+
+def all_nq_grids(app):
+    pass
+
+def all_sw_grids(app):
+    pass
